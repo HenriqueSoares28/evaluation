348a3ed9e6a8d81556ac54bdf8c7b7121d755afa
Henrique França Carvalho Soares <69248621+HenriqueSoares28@users.noreply.github.com>
2024-07-08 21:33:57 -0300

    Refactor run_superpixel_evaluation method in SuperpixelEvaluator class

--- python/minSpx.py
+++ python/minSpx.py
@@ -1,30 +1,38 @@
 import os
 import subprocess
-class Evaluation:        
-    def run_superpixel_evaluation(ext, img_path, gt_path, spx_map_path, save_path, min_spx_size):
-        # Create the save directory if it doesn't exist
-        os.makedirs(save_path, exist_ok=True)
-        
-        # Prepare the command with the given parameters
+
+class SuperpixelEvaluator:
+    def __init__(self, ext, img_path, gt_path, spx_map_path, save_path, min_spx_size):
+        self.ext = ext
+        self.img_path = img_path
+        self.gt_path = gt_path
+        self.spx_map_path = spx_map_path
+        self.save_path = save_path
+        self.min_spx_size = min_spx_size
+
+    def create_save_directory(self):
+        os.makedirs(self.save_path, exist_ok=True)
+
+    def build_command(self):
         params = [
-            "--ext", ext,
+            "--ext", self.ext,
             "--eval", "10",
-            "--img", img_path,
-            "--gt", gt_path,
-            "--rmsize", str(min_spx_size),
-            "--save", save_path,
-            "--label", spx_map_path
+            "--img", self.img_path,
+            "--gt", self.gt_path,
+            "--rmsize", str(self.min_spx_size),
+            "--save", self.save_path,
+            "--label", self.spx_map_path
         ]
+        return ["./bin/main"] + params
+
+    def run(self):
+        self.create_save_directory()
+        command = self.build_command()
         
-        command = ["./bin/main"] + params
-        
-        # Run the command
         result = subprocess.run(command, capture_output=True, text=True)
         
-        # Print the output and error (if any)
         print("Output:")
         print(result.stdout)
         if result.stderr:
             print("Error:")
             print(result.stderr)
-
